a742f05d99334125b98744cc1314fdcdc751881b
Youssef Sayed Elghool <gty@gmail.com>
2023-07-02 03:07:52 +0300

    Chang to text git commit command

---
+++ git commands .js
@@ -0,0 +1,34 @@
+
+
+cd // to go to path 
+cd G: // go to patation G
+
+dir // show the all file and folder in yoru posation 
+
+mkdir // mak a floder 
+
+git clone https://github.com/YoussefSayedElghool/Rally_Project.git  // clone the repo. from github to your pc in you curent place
+
+git status // git the status of your repo. and the new file that you add it in you local dir. but not add it to you to staging area 
+
+git add File.html index.html main.css // add this file to the staging area Note that this file befor the command dosenot in the staging area as you add it in you local dir. 
+ 
+git add * // add the all file that not not exist in staging area
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
